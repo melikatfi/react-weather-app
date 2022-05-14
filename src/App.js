7fd2acd7b6d94d98495beafe81ab7b783867245a
melikatfi <87155734+melikatfi@users.noreply.github.com>
2022-05-14 22:13:29 +0430

    loader added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Audio } from  'react-loader-spinner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Audio
+            height="100"
+            width="100"
+            color='grey'
+            ariaLabel='loading'
+        />        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
